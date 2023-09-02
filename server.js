e882918d1ce0dfc8d8a8a29864724def5358a3ce
Dada Teniola <42898812+emmy13@users.noreply.github.com>
2023-09-02 04:02:48 +0100

    basic project setup

--- server.js
+++ server.js
@@ -1,8 +1,53 @@
-//Stacks is an E-Library system aimed to provide not only the upmost requirements of an E-Library System but also top notch user experience like never before. Join us in the future of E-Libraries.
-
-//Stacks was created in partial participation of the below mentioned:
+//The below code implementation was performed in participation of:
 //Dada Teniola Emmanuel
 //Baiyere Samuel Fikayo
 
-//To initialize this project run "npm install", only on first install.
-//Then run either "node server.js" or "nodemon server.js" (if you have nodemon installed).
+//Initialize project dependencies
+require("dotenv").config();
+const express = require("express");
+const session = require('express-session');
+const path = require("path");
+const clientRoutes = require("./routes/clientRoutes")
+
+//Setup parameters
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Setup project middlewares
+//-----------------------------------------------------------------
+
+//Session
+var sess = {
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    sess.cookie.secure = true // serve secure cookies
+}
+app.use(session(sess))
+
+//Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "assets")));
+
+//EJS
+app.set("view engine", "ejs");
+app.set("views", "pages");
+
+//-----------------------------------------------------------------
+
+
+//Project Routes
+app.use(clientRoutes);
+
+
+//Start app and listen on a specified port
+app.listen(port, (err) =>
+  console.log(
+    err ||
+      `Localhost: http://localhost:${port}/\nServer listening on port ${port}`
+  )
+);
