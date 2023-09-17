eb1eb5213be5b5c9e689b4bfe48662cac1ee4550
Dada Teniola <42898812+emmy13@users.noreply.github.com>
2023-09-17 23:55:38 +0100

    changes

--- controllers/clientControllers.js
+++ controllers/clientControllers.js
@@ -1,9 +1,14 @@
 //Setup parameters
 
 //Setup route handlers
+const showIntroPage = (req, res) => {
+    res.render("intro")   
+}
+
 const showLandingPage = (req, res) => {
     res.render("landing")   
 }
 
 
-module.exports = { showLandingPage }
+
+module.exports = { showLandingPage, showIntroPage }
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,20 +1,14 @@
 //Setup project dependencies
 const {Router} = require("express");
-const { showLandingPage } = require("../controllers/clientControllers");
+const { showLandingPage, showIntroPage } = require("../controllers/clientControllers");
 
 
 //Setup parameters
 const router = Router();
 
-//Route middlewares
-//Set all route titles to "Stacks" by default
-router.use((req, res, next) => {
-    res.locals.title = 'Stacks';
-    next();
-})
 
 //Setup Routes
-router.get("/", showLandingPage);
+router.get("/", showIntroPage);
 
 
 module.exports = router;
